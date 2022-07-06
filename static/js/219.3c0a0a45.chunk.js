d7d00dcec718a74c55f1cacaeebd790e1a1f4aef
B0227555 <chandrakantsavvy@gmail.com>
2022-07-06 10:14:13 +0530

    Updates

---
+++ static/js/219.3c0a0a45.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksmart_meeting_organizer=self.webpackChunksmart_meeting_organizer||[]).push([[219],{4219:function(e,r,n){n.r(r);n(2791);var s=n(184);r.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h1",{children:"Some Error Occured"}),(0,s.jsx)("a",{href:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=219.3c0a0a45.chunk.js.map
